28e71002e70779a16fd642d234d76aa7e131dd13
comprefg <39472523+comprefg@users.noreply.github.com>
2022-08-26 18:23:37 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -19,7 +19,7 @@ curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
 sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
 sudo apt update
 sudo apt install playit
-
+ls
 
 
 #sudo apt install wget
