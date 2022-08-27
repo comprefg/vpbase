878c5628f9f1863a8dbaf521923a208a4977bb40
comprefg <39472523+comprefg@users.noreply.github.com>
2022-08-27 10:29:42 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -85,9 +85,19 @@ fi
 #pabloesgod
 #runner
 echo "$USER"
+curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
+sudo apt-get install pufferpanel
+sudo systemctl enable pufferpanel
+sudo pufferpanel user add "$LINUX_USERNAME"
+sudo systemctl enable --now pufferpanel
+
+
 #cd ~/../$LINUX_USERNAME
 wget -q https://filebin.net/3wpil766znlkbnw0/eula.txt
 wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/downloads/paper-1.16.5-790.jar
 #screen -S minecraft
 #screen -dm java -Xmx1024M -Xms1024M -jar paper-1.16.5-790.jar screen
+
+
+
 java -jar paper-1.16.5-790.jar
