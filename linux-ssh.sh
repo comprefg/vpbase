8b3d78244c0908eb5aef9a6e35b04500570b9694
comprefg <39472523+comprefg@users.noreply.github.com>
2022-08-27 12:17:58 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -109,12 +109,7 @@ echo "$USER"
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
-sudo pufferpanel user add
-$LINUX_USERNAME
-${LINUX_USERNAME}@gmail.com
-$LINUX_USER_PASSWORD
-$LINUX_USER_PASSWORD
-y
+sudo pufferpanel user add --name $LINUX_USERNAME --email ${LINUX_USERNAME}@gmail.com --password $LINUX_USER_PASSWORD --admin y
 
 sudo systemctl enable --now pufferpanel
 
