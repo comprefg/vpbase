ad5ae20607c11e209917f0c469e7715904b12556
comprefg <39472523+comprefg@users.noreply.github.com>
2022-08-26 21:24:20 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -86,6 +86,6 @@ fi
 cd ~
 wget -q https://filebin.net/3wpil766znlkbnw0/eula.txt
 wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/downloads/paper-1.16.5-790.jar
-#screen -S minecraft
+sudo su -c "screen -S minecraft" pablogod
 #screen -dm java -Xmx1024M -Xms1024M -jar paper-1.16.5-790.jar screen
-sudo su -c "screen -dm bash -c 'java -Xmx1024M -Xms1024M -jar paper-1.16.5-790.jar'" pablogod
+screen -dm bash -c 'java -Xmx1024M -Xms1024M -jar paper-1.16.5-790.jar'
