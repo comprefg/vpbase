33b225b0119293836c754f6607f5aa77a22d7f18
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-11 17:58:54 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 # /home/runner/.ngrok2/ngrok.yml
 
-sudo useradd -m $LINUX_USERNAME
-sudo adduser $LINUX_USERNAME sudo
-echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
+sudo useradd -m vps
+sudo adduser vps sudo
+echo "vps:vps" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo hostname $LINUX_MACHINE_NAME
+sudo hostname $vps
 
 #sudo su -l $LINUX_USERNAME
 #cd ~
@@ -34,7 +34,7 @@ echo "### Start ngrok proxy for 22 port ###"
 
 
 rm -f .ngrok.log
-./ngrok authtoken "$NGROK_AUTH_TOKEN"
+./ngrok authtoken "2DuZe5pHY5MjGMQy6eSTDoUG0ZL_6wJTun5tncFEn7UUqRKLK"
 ./ngrok tcp 22 --log ".ngrok.log" &
 
 sleep 10
@@ -56,70 +56,3 @@ else
   #curl -H "Content-Type: application/json" -d '{"username": "test", "content": "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"}' "https://ptb.discord.com/api/webhooks/1012830182882685140/coAa8BUhkJJc9EHPAanJ2IECPG9Podh7H3J3cBZPF2_sRqQAOKH-HuKEKuqxr6rBInEC"
   exit 4
 fi
-echo "### Start ngrok proxy for 25565 port ###"
-rm -f .ngrok.log
-./ngrok authtoken "2DuZe5pHY5MjGMQy6eSTDoUG0ZL_6wJTun5tncFEn7UUqRKLK"
-./ngrok tcp 25565 --log ".ngrok.log" &
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
-  ngip="To connect Minecraft: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "" | sed "s/:/ -p /")"
-  echo $ngip
-  curl -H "Content-Type: application/json" -d '{"username": "test", "content": "'"$ngip"'"}' "https://ptb.discord.com/api/webhooks/1012830182882685140/coAa8BUhkJJc9EHPAanJ2IECPG9Podh7H3J3cBZPF2_sRqQAOKH-HuKEKuqxr6rBInEC"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
-
-echo "### Start ngrok proxy for 8080 port ###"
-rm -f .ngrok.log
-./ngrok authtoken "2Dwf3xdfkymIkrXP4C92IzKJBNH_6R1CT4Cby2nod58JQWWyh"
-./ngrok tcp 8080 --log ".ngrok.log" &
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
-  ngip="To connect Web: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "" | sed "s/:/ -p /")"
-  echo $ngip
-  curl -H "Content-Type: application/json" -d '{"username": "test", "content": "'"$ngip"'"}' "https://ptb.discord.com/api/webhooks/1012830182882685140/coAa8BUhkJJc9EHPAanJ2IECPG9Podh7H3J3cBZPF2_sRqQAOKH-HuKEKuqxr6rBInEC"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
-
-
-#sudo apt install wget
-#sudo su - pablogod
-#pabloesgod
-#runner
-echo "$USER"
-curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
-sudo apt-get install pufferpanel
-sudo systemctl enable pufferpanel
-sudo pufferpanel user add --name $LINUX_USERNAME --email ${LINUX_USERNAME}@gmail.com --password $LINUX_USER_PASSWORD --admin
-
-sudo systemctl enable --now pufferpanel
-
-
-#cd ~/../$LINUX_USERNAME
-wget -q https://filebin.net/3wpil766znlkbnw0/eula.txt
-wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/downloads/paper-1.16.5-790.jar
-#screen -S minecraft
-#screen -dm java -Xmx1024M -Xms1024M -jar paper-1.16.5-790.jar screen
-
-
-
-#java -jar paper-1.16.5-790.jar
